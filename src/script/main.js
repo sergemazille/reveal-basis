7960b2b2bc3e42e3f7df2515fb61c255d089e5fc
Serge Mazille <serge.mazille@gmail.com>
2017-08-19 09:11:36 +0200

    Added element.closest polyfill

--- src/script/main.js
+++ src/script/main.js
@@ -2,12 +2,17 @@
 import '../style/reveal-basis.scss';
 
 // scripts imports
+import * as utils from './utils';
+
 import { Overlay } from './components/overlay';
 import { Spinner } from './components/spinner';
 
 import { Above } from './components/above';
 import { Notification } from './components/notification';
 import { Drop } from './components/drop';
+
+// add polyfills if need be
+utils.polyfills();
 
 // unique components creation
 new Overlay().init();
--- src/script/utils.js
+++ src/script/utils.js
@@ -7,3 +7,31 @@ export function classListIncludes(classList, string) {
 export function getCssDuration(milliseconds) {
     return (milliseconds / 1000 + 's');
 }
+
+// polyfills
+export function polyfills() {
+
+    // 'element.closest'
+    if (typeof HTMLElement.prototype.closest === "undefined") {
+        polyfillElementClosest();
+    }
+}
+
+// see https://developer.mozilla.org/fr/docs/Web/API/Element/closest#Proth%C3%A8se_d'%C3%A9mulation_(polyfill)
+function polyfillElementClosest() {
+    HTMLElement.prototype.closest = function () {
+        if (window.Element && !Element.prototype.closest) {
+            Element.prototype.closest =
+                function(s) {
+                    let matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                        i,
+                        el = this;
+                    do {
+                        i = matches.length;
+                        while (--i >= 0 && matches.item(i) !== el) {};
+                    } while ((i < 0) && (el = el.parentElement));
+                    return el;
+                };
+        }
+    }
+}
